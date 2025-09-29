44af0d06119f54029d8ec1fc35ce7549ffbf8820
jquinzell <>
2025-09-29 17:59:49 -0500

    make tensorflow external to fix build issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverComponentsExternalPackages: ['@tensorflow/tfjs-node'],
+  experimental: {
+    serverComponentsExternalPackages: ['@tensorflow/tfjs-node'],
+  },
 }
 
 export default nextConfig
